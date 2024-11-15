3d6c6e2d8a272fd458f2dced58652a065387dbeb
Sara <sarailic431@gmail.com>
2024-11-15 21:50:36 +0100

    starting to do list functionality

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+"use strict";
 import "./style.css";
+
+const inputItem = document.querySelector(".input-name");
+let inputName;
+
+inputItem.addEventListener("input", function (e) {
+  inputName = e.target.value;
+  console.log(inputName);
+});
