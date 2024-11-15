5f07e186a00bf0070c590151b0ada67d3e817edd
Sara <sarailic431@gmail.com>
2024-11-15 21:55:07 +0100

    testing functionalty

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 "use strict";
 import "./style.css";
 
+const container = document.querySelector(".container");
+const form = document.querySelector(".form");
 const inputItem = document.querySelector(".input-name");
+const addBtn = document.querySelector(".add-item");
 let inputName;
 
 inputItem.addEventListener("input", function (e) {
   inputName = e.target.value;
   console.log(inputName);
+});
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // form.style.backgroundColor = "red";
 });
