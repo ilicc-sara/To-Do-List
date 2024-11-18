03c6cc6360f2cca08cd3bac6d9d31880f41f95c1
Sara <sarailic431@gmail.com>
2024-11-18 13:29:41 +0100

    improving overall functionality with html data, css and JS

--- main.js
+++ main.js
@@ -16,26 +16,38 @@ const toDo = [];
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+  const newToDo = { id: crypto.randomUUID(), name: inputName, isDone: false };
+  toDo.push(newToDo);
 
   const newInput = document.createElement("div");
   newInput.classList.add("input-item");
+
+  newInput.setAttribute("data-id", newToDo.id);
+
   newInput.innerHTML = `${inputName} <button class="btn-done">DONE</button>`;
   // container.append(newInput);
+  // container.insertAdjacentElement("beforeend", newInput);
   container.insertAdjacentElement("afterend", newInput);
   inputItem.value = "";
 
-  toDo.push({ name: inputName, isDone: false });
-
-  toDo.forEach(function (item, i) {
-    item.id = i + 1;
-  });
-  console.log(toDo);
+  // toDo.forEach(function (item, i) {
+  //   item.id = i + 1;
+  // });
+  // console.log(toDo);
 
   const btnDoneEl = document.querySelectorAll(".btn-done");
 
   btnDoneEl.forEach(function (btn, i) {
     btn.addEventListener("click", function (e) {
-      // console.log(e.target);
+      // console.log(e.target.getAttribute(id));
+      // console.log(e.target.parentElement.dataset.id);
+      // console.log(newToDo.id);
+
+      if (e.target.parentElement.dataset.id === newToDo.id) {
+        newToDo.isDone = true;
+        console.log(newToDo);
+        console.log(toDo);
+      }
       e.currentTarget.parentElement.style.backgroundColor = "green";
     });
   });
