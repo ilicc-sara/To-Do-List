c04b7495dc65733bda1977b478950de3b800a5e8
Sara <sarailic431@gmail.com>
2024-11-21 20:10:50 +0100

    closure

--- main.js
+++ main.js
@@ -17,43 +17,77 @@ inputItem.addEventListener("input", function (e) {
 const toDo = [];
 let newToDo;
 
+const toDoCreator = function () {
+  const toDo = { id: crypto.randomUUID(), name: "", isDone: false };
+
+  const getToDo = () => toDo;
+  const editToDoName = (newName) => (toDo.name = newName);
+  const editIsDone = (newStatus) => (toDo.isDone = newStatus);
+
+  return { getToDo, editToDoName, editIsDone };
+};
+
+const toDoManagerCreator = function () {
+  const toDos = [];
+
+  const getToDos = () => toDos;
+  const addToDo = (toDo) => toDos.push(toDo);
+  return { addToDo, getToDos };
+};
+
+const toDoManager = toDoManagerCreator();
+// console.log(toDoManager);
+// console.log(toDoManager.getToDos());
+// toDoManager.addToDo({ name: "Sara" });
+// console.log(toDoManager.getToDos());
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   if (!inputItem.value) return;
-  newToDo = { id: crypto.randomUUID(), name: inputName, isDone: false };
-  toDo.push(newToDo);
+  // newToDo = { id: crypto.randomUUID(), name: inputName, isDone: false };
+  const newToDo = toDoCreator();
+  toDoManager.addToDo(newToDo);
+
+  toDoManager.getToDos().forEach((toDo) => console.log(toDo.getToDo().id));
 
   const newInput = document.createElement("li");
   newInput.classList.add("input-item");
 
-  newInput.setAttribute("data-id", newToDo.id);
+  newInput.setAttribute("data-id", newToDo.getToDo().id);
 
   newInput.innerHTML = `${inputName} <button class="btn-done">DONE</button>
-  <btn class="close"
-    ><ion-icon class="x-icon" name="close-outline"></ion-icon
+  <btn id="close" class="close"
+    ><ion-icon id="close" class="x-icon" name="close-outline"></ion-icon
   ></btn>`;
 
   // container.insertAdjacentElement("afterbegin", newInput);
   toDoList.appendChild(newInput);
 
   inputItem.value = "";
-
-  const btnDoneEl = document.querySelectorAll(".btn-done");
 });
 
 toDoList.addEventListener("click", function (e) {
   if (e.target.classList.contains("btn-done")) {
     // console.log(e.target.closest(".input-item").dataset.id);
-    console.log(toDo);
-    let targetEl = toDo.find(
-      (x) => x.id === e.target.closest(".input-item").dataset.id
-    );
-    targetEl.isDone = true;
-    console.log(targetEl);
+
+    // let targetEl = toDo.find(
+    //   (x) => x.id === e.target.closest(".input-item").dataset.id
+    // );
+    // targetEl.isDone = true;
+    const parentToDoId = e.target.closest(".input-item").dataset.id;
+    const currentToDo = toDoManager
+      .getToDos()
+      .find((toDo) => toDo.getToDo().id === parentToDoId);
+    console.log("current to do", currentToDo.getToDo());
+    console.log("parent to do", parentToDoId);
+
+    currentToDo.editIsDone(true);
+    console.log("current to do", currentToDo.getToDo());
+
     e.target.closest(".input-item").style.backgroundColor = "green";
 
     // e.target.closest(".input-item").querySelector(".btn-done").remove();
-  } else if (e.target.classList.contains("close")) {
+  } else if (e.target.id === "close") {
     let indexEl = toDo.findIndex(
       (x) => x.id === e.target.closest(".input-item").dataset.id
     );
@@ -64,3 +98,7 @@ toDoList.addEventListener("click", function (e) {
     deleteEL.remove();
   }
 });
+
+// napravi tri countera svaki counter neka bude iniciran sa 0
+// na ekranu neka budu dva btn izmedju kojih ce biti vrednost countera
+// pritiskom na levo povecava se , a na desno
